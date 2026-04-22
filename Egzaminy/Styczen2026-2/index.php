7cf7dcdcdbe767e77b2c18f631a6dce36a567b25
HAQ20 <139398720+HAQ20@users.noreply.github.com>
2026-04-22 08:55:48 +0200

    22.04.2026v2
    Egzamin 2 skończony

--- Egzaminy/Styczen2026-2/index.php
+++ Egzaminy/Styczen2026-2/index.php
@@ -7,37 +7,54 @@
     <link rel="stylesheet" href="styl.css">
 </head>
 <body>
-    <?php
-    $mysqli = new mysqli("localhost", "root", "", "bazar");
-    ?>
-    <nav>
+    <header>
         <h1>Zdrowy bazarek</h1>
+    </header>
+    <nav>
         <?php
-        $result = $mysqli->query("SELECT plik, rodzaj FROM towar");
+        $mysqli = new mysqli("localhost", "root", "", "bazar");
+        $result = $mysqli->query("SELECT plik, rodzaj, nazwa FROM towar WHERE rodzaj='owoc'");
         while($row = $result->fetch_assoc()){
-            echo "<img src='pliki3-1/".$row['plik']."' alt='".$row['rodzaj']."'>";
+            echo "<img src='pliki3-1/".$row['plik']."' alt='".$row['nazwa']."'>";
         }
         ?>    
         </nav>
-    <div class="boczny">
-        <img src="pliki3-1/market.png" alt="bazarek">
-    </div>
-    <section>
-        <p>Wybierz owoc lub warzywo i podaj wagę</p>
-        <form action="skrypt.php" method="post">
-            <select name="produkt" id="produkt"></select>
-            <input type="number" name="waga" id="waga" step="0.01" required>
-            <input type="submit" value="Zamów">
-        </form>
-        <?php
-            $mysqli = new mysqli("localhost", "root", "", "bazar");
-            if ($_POST['produkt'] && $_POST['waga']){
+    <main class="content">
+        <div class="boczny">
+            <img src="pliki3-1/market.png" alt="bazarek">
+        </div>
+        <section>
+
+            <p>Wybierz owoc lub warzywo i podaj wagę</p>
+            <form method="post">
+                <select name="produkt" id="produkt">
+                <?php
+                $result = $mysqli->query("SELECT nazwa, id FROM towar");
+                while($row = $result->fetch_assoc()){
+                    echo "<option value='".$row['id']."'>".$row['nazwa']."</option>";
+                }    
+                ?>
+                </select>
+                <input type="number" name="waga" id="waga" step="0.01" required>
+                <input type="submit" value="Zamów" name='zamow'>
+            </form>
+            <?php
+            if(isset($_POST['zamow'])){
                 $produkt = $_POST['produkt'];
                 $waga = $_POST['waga'];
-
+                $result = $mysqli->query("SELECT rodzaj, nazwa, cena FROM towar WHERE id=".$produkt);
+                $row = $result->fetch_assoc();
+                $rodzaj = $row['rodzaj'];
+                $nazwa = $row['nazwa'];
+                $cena_za_kg = $row['cena'];
+                $cena = $cena_za_kg * $waga;
+                echo "<p>".$rodzaj." ".$nazwa." wartość: ".number_format($cena, 2)." zł</p>";
+                $mysqli->query("INSERT INTO zamowienia (id_towaru, waga) VALUES (".$produkt.", ".$waga.")");
+                $mysqli->close();
             }
-        ?>
-    </section>
+            ?>
+        </section>
+    </main>
     <footer>Stronę opracował: 00000000</footer>
 </body>
 </html>
